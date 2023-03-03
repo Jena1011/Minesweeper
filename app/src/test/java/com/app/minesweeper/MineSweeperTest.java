91408830268200b9d43cad40feed6c3d2dea91c2
Jena1011 <118824973+Jena1011@users.noreply.github.com>
2023-03-03 22:04:32 +0800

    refactor 利用圖示字串做測試(視覺化)

--- app/src/test/java/com/app/minesweeper/MineSweeperTest.java
+++ app/src/test/java/com/app/minesweeper/MineSweeperTest.java
@@ -31,8 +31,11 @@ public class MineSweeperTest {
     //檢驗：點擊格子，變成打開狀態
     @Test
     public void tapCellShouldOpen(){
-        ArrayList<Cell> cells = new ArrayList<>();
-        cells.add(new Cell(0,0));
+//        ArrayList<Cell> cells = new ArrayList<>();
+//        cells.add(new Cell(0,0));
+        ArrayList<String> init = new ArrayList<>();
+        init.add("-");
+        ArrayList<Cell> cells = createCell(init);
 
         ICellCreator creator = new FakeCellCreator();
         ((FakeCellCreator) creator).cells = cells;
@@ -42,13 +45,49 @@ public class MineSweeperTest {
         int y = 0;
 
         mineSweeper.tap(x,y);
-        Cell cell = mineSweeper.getCell(x, y);
-        Assert.assertEquals(Cell.STATUS.OPEN,cell.status);
 
+        ArrayList<String> verify = new ArrayList<>();
+        verify.add(" ");
+        verifyDisplay(verify);
 
     }
 
+    // 利用圖示字串做驗證
+    private void verifyDisplay(ArrayList<String> verify) {
+        for (int x=0; x<verify.size(); x++) {
+            String[] ylist = verify.get(x).split("\\|");
+            for(int y=0; y<ylist.length; y++){
+                String value = ylist[y];
+                Cell findCell = mineSweeper.getCell(x,y);
+                switch (value){
+                    case " ":
+                        Assert.assertEquals("$x, $y", Cell.STATUS.OPEN, findCell.status);
+                        break;
+                    case "-":
+                        Assert.assertEquals("$x, $y", Cell.STATUS.CLOSE, findCell.status);
+                        break;
+                }
+            }
+        }
+    }
 
+    // 將圖示字串轉為 ArrayList<Cell>
+    private ArrayList<Cell> createCell(ArrayList<String> initSweeper) {
+        ArrayList<Cell> cells = new ArrayList<>();
+        for (int x=0; x<initSweeper.size(); x++) {
+            String[] ylist = initSweeper.get(x).split("|");
+            for(int y=0; y<ylist.length; y++){
+                Cell cell = new Cell(x,y);
+                String value = ylist[y];
+                cell.status = Cell.STATUS.CLOSE;
+                if(value==" "){
+                    cell.status = Cell.STATUS.OPEN;
+                }
+                cells.add(cell);
+            }
+        }
+        return cells;
+    }
 
 
     //檢驗：點擊格子，若為周圍炸彈數量為0，自動打開周圍格子
